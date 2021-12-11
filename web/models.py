b665888e459f434968a7b45eb5fc0941ab0b2774
valbendan <valbendan@hotmail.com>
2021-12-11 21:56:31 +0800

    format code

--- web/models.py
+++ web/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 # Create your models here.
 
+
 class DemoModel(models):
-    title = models.TextField(verbose_name='w')
+    title = models.TextField(verbose_name="w")
     world: str = models.CharField(max_length=23)
 
     def cool(self) -> "DemoModel":
